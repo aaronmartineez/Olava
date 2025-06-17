7406b83ff1d35ed00f4d725332310a2a4b19079c
Aaron Martinez <aaronmartineez@gmail.com>
2025-06-16 23:30:21 -0300

    actualizacion

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
+export interface iTrack {
+  id: number,
+  title: string,
+  duration: number,
+  artist: string
+}
+
+export const tracks: iTrack[] = [
+  {
+    id: 1, 
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+  {
+    id: 2, 
+    title: "cancion 2",
+    duration: 130,
+    artist: "interprete 2"
+  },
+  {
+    id: 3, 
+    title: "cancion 3",
+    duration: 220,
+    artist: "interprete 3"
+  }
+]
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTracks(): iTrack[] {
+    return tracks;
   }
 }
